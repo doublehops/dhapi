8bb98ca7058024e84893a2ccf382dc682fb91125
Damien Buttler <damien@doublehops.com>
2023-09-29 11:54:19 +1000

    Added email address validation.

---
+++ validator/emailaddress_test.go
@@ -0,0 +1,48 @@
+package validator
+
+import "testing"
+
+func TestEmailAddress(t *testing.T) {
+	tests := []struct {
+		name           string
+		value          string
+		required       bool
+		expectedResult bool
+		expectedError  string
+	}{
+		{
+			name:           "validEmailAddress",
+			value:          "test@example.com",
+			required:       true,
+			expectedResult: true,
+			expectedError:  "",
+		},
+		{
+			name:           "invalidEmailAddressButNotRequired",
+			value:          "test.last",
+			required:       false,
+			expectedResult: false,
+			expectedError:  EmailAddressDefaultMessage,
+		},
+		{
+			name:           "emptyEmailAddressButNotRequired",
+			value:          "",
+			required:       false,
+			expectedResult: true,
+			expectedError:  "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			validator := EmailAddress(tt.expectedError)
+			res, err := validator(tt.required, tt.value)
+			if tt.expectedResult != res {
+				t.Errorf("test result not as expected. Expected: %t; got: %t", tt.expectedResult, res)
+			}
+			if tt.expectedError != err {
+				t.Errorf("test result not as expected. Expected: %s; got: %s", tt.expectedError, err)
+			}
+		})
+	}
+}
