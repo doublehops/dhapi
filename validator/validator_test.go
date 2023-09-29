bc73601c2188928766f69364ffbb73b86de77a83
Damien Buttler <damien@doublehops.com>
2023-09-29 11:41:55 +1000

    Added test for Validation function.

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -1,59 +1,72 @@
 package validator
 
-//type Car struct {
-//	Make  string
-//	Model string
-//	Year  int
-//}
-//
-//func TestValidate(t *testing.T) {
-//	tests := []struct {
-//		Name           string
-//		Rules          []Rule
-//		Payload        Car
-//		expectedErrors []ErrorMessage
-//	}{
-//		{
-//			Name: "AllPass",
-//			Rules: []Rule{
-//				{
-//					VariableName: "Name",
-//					Required:     true,
-//					Function:     Min(1, 2)(1, 2),
-//				},
-//			},
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.Name, func(t *testing.T) {
-//
-//			v := []Rule{}
-//			val := Min()(1, 2)
-//			a := val(1, 2)
-//
-//			//for _, rule := range tt.Rules {
-//			//	rule.Value = tt.Payload.
-//			//	{
-//			//		rule.VariableName
-//			//	}
-//			//}
-//			//errors := Validate("tt.Payload", tt.Rules)
-//			//if !reflect.DeepEqual(tt.expectedErrors, errors) {
-//			//	t.Errorf("Unexpected validation response. Expected: %v; Actual: %v", tt.expectedErrors, errors)
-//			//}
-//		})
-//	}
-//}
-//
-//func validations(car Car) []Rule {
-//	return []Rule{
-//		{"make", car.Make, true, NotEmptyAAA("asd", true)},
-//	}
-//}
-//
-//func (c *Car) Validate() []ErrorMessage {
-//	var errors []ErrorMessage
-//
-//	return errors
-//}
+import (
+	"reflect"
+	"testing"
+)
+
+type Car struct {
+	Make  string
+	Model string
+	Year  int
+}
+
+func TestValidate(t *testing.T) {
+	tests := []struct {
+		name  string
+		rules []Rule
+		//payload        Car
+		expectedErrors ErrorMessages
+	}{
+		{
+			name: "allPass",
+			rules: []Rule{
+				{
+					VariableName: "Make",
+					Required:     true,
+					Value:        "Ford",
+					Function:     []ValidationFunctions{MinLength(4, "")},
+				},
+			},
+			expectedErrors: ErrorMessages{},
+		},
+		{
+			name: "makeFail",
+			rules: []Rule{
+				{
+					VariableName: "Make",
+					Required:     true,
+					Value:        "Ford",
+					Function:     []ValidationFunctions{MinLength(10, "")},
+				},
+			},
+			expectedErrors: ErrorMessages{
+				"Make": {MinLengthDefaultMessage},
+			},
+		},
+		{
+			name: "makeFailWithCustomError",
+			rules: []Rule{
+				{
+					VariableName: "Make",
+					Required:     true,
+					Value:        "Ford",
+					Function:     []ValidationFunctions{MinLength(10, "MyCustomerErrorMessage")},
+				},
+			},
+			expectedErrors: ErrorMessages{
+				"Make": {"MyCustomerErrorMessage"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			errors := RunValidation(tt.rules)
+			if !reflect.DeepEqual(tt.expectedErrors, errors) {
+				t.Errorf("Error not as expected. Expected: %v; got: %v", tt.expectedErrors, errors)
+			}
+		})
+	}
+}
