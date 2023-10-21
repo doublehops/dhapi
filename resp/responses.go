af0dc70b2764c75be064f95012ca28121df4f256
Damien Buttler <damien@doublehops.com>
2023-10-21 18:58:31 +1100

    Better error messages.

--- resp/responses.go
+++ resp/responses.go
@@ -1,11 +1,31 @@
 package resp
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+)
+
+var (
+	ValidationError    = errors.New("one or more validation errors occurred")
+	CouldNotSaveRecord = errors.New("could not save record")
+
+	NotFoundMsg = ErrorMessage{"message": "not found"}
+)
+
+type ErrorMessage map[string]string
 
 func GetSingleItemResp(data interface{}) SingleItemResp {
 	return SingleItemResp{
 		Data: data,
 	}
+}
+
+func GetNotFoundResp() ErrorMessage {
+	return ErrorMessage{"message": "not found"}
+}
+
+func ErrorResp(msg string) CustomErrorResp {
+	return CustomErrorResp{Message: msg}
 }
 
 func GetListResp(data interface{}, pagination Pagination) ListResp {
@@ -15,10 +35,17 @@ func GetListResp(data interface{}, pagination Pagination) ListResp {
 	}
 }
 
-func GetValidateErrResp(errors ErrMsgs) ValidateErrResp {
+// GetValidateErrResp will prepare the error response. It will default to a predefined error for Message but
+// will override it if one is supplied.
+func GetValidateErrResp(errors ErrMsgs, errs ...string) ValidateErrResp {
+	err := ValidationError.Error()
+	if len(errs) > 0 {
+		err = errs[0]
+	}
+
 	return ValidateErrResp{
 		Name:    "Validation failed",
-		Message: "One or more validation errors occurred",
+		Message: err,
 		Code:    http.StatusBadRequest,
 		Status:  "error",
 		Errors:  errors,
